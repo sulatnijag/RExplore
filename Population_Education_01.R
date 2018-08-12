02ae80c9e35591994f3fc0884c09b09de0e62019
Jag <sulatnijag@gmail.com>
2018-08-12 17:19:47 +0800

    Wrangling logic updated

--- Population_Education_01.R
+++ Population_Education_01.R
@@ -11,6 +11,18 @@ pol <- read_excel("Data/Population_Education.xlsx", sheet="Polytechnic", col_nam
 pol$school <- "Polytechnic"
 
 pop_edu <- rbind_all(list(uni, pol)) %>% arrange(Course)
-
 pop_edu <- pop_edu %>% extract(Course, c("Gender", "Course"), "(.*):[[:space:]](.*)")
 
+cols <- colnames(pop_edu)
+cols <- cols[! cols %in% c("Gender","school", "Course") ]
+
+pop_edu <- pop_edu %>% mutate_at(vars(cols), funs(as.numeric(.)) )
+
+filter(pop_edu , grepl("information technology", pop_edu$Course, ignore.case=T)) %>% 
+  select(-Gender,-school) %>%
+  group_by(Course) %>% summarise_all(sum) %>% View()
+
+
+
+
+          
