5116028b6e050735738b8312f51db1222d8b4f54
Jag <sulatnijag@gmail.com>
2018-08-12 16:22:30 +0800

    Split field into two

--- Population_Education_01.R
+++ Population_Education_01.R
@@ -1,4 +1,16 @@
-library(xlsx)
+library(tidyverse)
+library(readxl)
 
 
-uni <- read.xlsx("Data/Population_Education.xlsx", sheet="University")
+rm(list=ls())
+
+uni <- read_excel("Data/Population_Education.xlsx", sheet="University", col_names=T)
+uni$school <- "University"
+
+pol <- read_excel("Data/Population_Education.xlsx", sheet="Polytechnic", col_names=T)
+pol$school <- "Polytechnic"
+
+pop_edu <- rbind_all(list(uni, pol)) %>% arrange(Course)
+
+pop_edu <- pop_edu %>% extract(Course, c("Gender", "Course"), "(.*):[[:space:]](.*)")
+
